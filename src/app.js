ede449362f7788344318a0349a6f64f4faff9664
Manu-rca-git <Manuelramoncarrasco05@gmail.com>
2024-11-06 18:52:39 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "Mi perro",
+    "El gato del vecino",
+    "Mi hermano pequeño",
+    "El cartero"
+  ];
+  let action = ["se comió", "derribó", "rompió", "perdió"];
+  let what = ["mi tarea", "mi computadora", "el control remoto", "mi paquete"];
+  let when = [
+    "ayer por la tarde",
+    "esta mañana",
+    "durante la cena",
+    "la semana pasada"
+  ];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  let excuse =
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen];
+
+  document.querySelector("#excuse").innerHTML = excuse;
 };
